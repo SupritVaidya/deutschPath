6a63f15261880e83ec333a886dc5730734f3c87d
Suprit Vaidya <suprit.vaidya@gmail.com>
2025-11-05 13:57:10 +0200

    api links for localhost

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -16,7 +16,8 @@ type LoginResponse = {
 };
 
 export async function signup(name: string, email: string, password: string): Promise<User> {
-  const resp = await apiFetch("https://deutschpath-euhufrdpdcbreqfg.uksouth-01.azurewebsites.net/api/Users", {
+  const resp = await apiFetch("https://localhost:7114/api/Users", {
+  //const resp = await apiFetch("https://deutschpath-euhufrdpdcbreqfg.uksouth-01.azurewebsites.net/api/Users", {
     method: "POST",
     body: JSON.stringify({ name, email, password }),
     auth: false
@@ -37,7 +38,8 @@ export async function signup(name: string, email: string, password: string): Pro
 }
 
 export async function login(email: string, password: string): Promise<User> {
-  const resp = await apiFetch("https://deutschpath-euhufrdpdcbreqfg.uksouth-01.azurewebsites.net/api/auth/login", {
+  const resp = await apiFetch("https://localhost:7114/api/auth/login", {
+  //const resp = await apiFetch("https://deutschpath-euhufrdpdcbreqfg.uksouth-01.azurewebsites.net/api/auth/login", {
     method: "POST",
     body: JSON.stringify({ email, password }),
     auth: false
